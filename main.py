fabe37bdc2a436bd28749d76e1f313873c5f2cf0
Ting Ting Huang <thschool2@gmail.com>
2017-07-25 09:13:01 -0700

    added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#WHEN YOU DON'T KNOW WHAT TO IMPORT
 import webapp2
 import jinja2
 import logging
@@ -13,12 +14,41 @@ jinja_environment = jinja2.Environment(
     loader = jinja2.FileSystemLoader(
     os.path.dirname(__file__) + '/templates'))
 
+#OUR STATIC CLASSES!!!!!!!
+class Byte(ndb.Model):
+    byte = ndb.StringProperty()
+
+#OUR HANDLERS THAT ACTUALLY DO SHIT
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('templates/main.html')
+        template = jinja_environment.get_template('main.html')
         self.response.out.write(template.render())
 
 
 app = webapp2.WSGIApplication([
+    ('/', MainHandler),], debug=True)
+
+class CreateByte(webapp2.RequestHandler):
+    def post(self):
+        byte_key = ndb.Key('Byte', self.request.get("byte"))
+        byte = byte_key.get()
+        if not byte:
+            byte = Byte(byte = self.request.get('byte'))
+            byte.key = byte_key
+            byte.put()
+        self.redirect('/view.html')
+
+class View(webapp2.RequestHandler):
+    def get(self):
+        query = Byte.query().order(Byte.byte)
+        byte_s = query.fetch()
+        template = jinja_environment.get_template('view.html')
+        var = {'byte':byte_s}
+        self.response.out.write(template.render(var))
+
+app = webapp2.WSGIApplication([
     ('/', MainHandler),
-], debug=True)s
+    ('/create_byte', CreateByte),
+    ('/view.html', View),
+    ('/main.html', MainHandler),
+], debug=True)
