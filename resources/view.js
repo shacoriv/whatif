afbe327488383c2d5e208377b88f5190331ee553
Tommy Nguyen <Nnguyen.h.tommy@gmail.com>
2017-07-25 11:15:49 -0700

    I added the comment box

--- resources/view.js
+++ resources/view.js
@@ -1,9 +1,17 @@
-function blur() {
-  $('#footer').blur();
-}
+function open(){
+  console.log("Please open");
+  $('#lightbox').fadeTo(1000, 1);
+  $("#wrapper").css({'text-shadow': '0px 0px 10px #000'});
+};
 
-function button() {
-  $('#footer').click(blur);
-}
+function close(){
+  console.log("hello world");
+  $('#lightbox').hide();
+  $("#wrapper").css({'text-shadow': '0px 0px 0px #000'});
+};
 
-$(document).ready(button);
+
+$(document).ready(function (){
+  $('#open').on("click",open);
+  $('#close').on("click",close);
+})
